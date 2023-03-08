6ffaddda61a7ea0ff1410e5d7134f9469e3e5300
João Pedro Oliveira <126018501+jo4o-art@users.noreply.github.com>
2023-03-07 22:30:06 -0300

    criei ex 8, 9, 10

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 12
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+
+if (vel > 60) {
+    console.log(`Multado! vc ultrapasou a velocidade permitida`)
+} 
+
+console.log(`Dirija sempre usando sinto de segurança`)
---
+++ aula11/ex009.js
@@ -0,0 +1,6 @@
+var país = 'Brasil'
+if (país == 'Brasil') {
+    console.log('vc é Brasileiro!')
+} else {
+    console.log('vc é Estrangeiro!')
+}
