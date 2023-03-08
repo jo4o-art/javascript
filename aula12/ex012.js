07f9ff3f2c6c3d734f9e5806a0a7b6af833d07ba
João Pedro Oliveira <126018501+jo4o-art@users.noreply.github.com>
2023-03-08 18:43:43 -0300

    commit

---
+++ aula12/ex012.js
@@ -0,0 +1,10 @@
+var hora = 18
+
+console.log(`Agora são exatamente ${hora} Horas`)
+if (hora < 12) {
+    console.log(`Bom Dia`)
+} else if (hora >= 12 && hora <= 17) {
+    console.log(`Boa Tarde`)
+} else {
+    console.log(`Boa Noite`)
+}
