108b51a89cb9d1388dd786470e6c7a7faacf32f0
João Pedro Oliveira <126018501+jo4o-art@users.noreply.github.com>
2023-03-17 13:39:22 -0300

    finalizai aula16

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -1,6 +1,6 @@
-let num = [5, 8, 2, 9, 3]
+let num = [5, 8, 2, 9, 3, 1, 4, 6, 7]
 num.sort()
-num.push()
+num.push();
 console.log(`${num}`)
 console.log(`o vetor tem ${num.length} posições`)
 console.log(`o primeiro valor do vetor é ${num[0]}`)
