97b36ec64b3e5c62ebb5a4888323028ba016e873
João Pedro Oliveira <126018501+jo4o-art@users.noreply.github.com>
2023-08-21 21:32:49 -0300

    tipos primitivos

---
+++ brasilian-dev/tipos-primitivos.js
@@ -0,0 +1,19 @@
+let meuNumero = 10;
+let minhaString = "Helo Word!"
+let meuBoolean = true
+let meuNull = null
+let meuUndefined = undefined
+
+console.log('meu número é: ', meuNumero)
+console.log('minha string é:', minhaString)
+console.log('meu boolean é:', meuBoolean)
+console.log('meu Null é:', meuNull)
+console.log('meu undefined é: ', meuUndefined)
+
+console.log('')
+
+console.log('o tipo do meu número é: ', typeof meuNumero)
+console.log('o tipo da minha string é: ', typeof minhaString)
+console.log('o tipo da meu boolean é: ', typeof meuBoolean)
+console.log('o tipo da meu null é: ', typeof meuNull)
+console.log('o tipo da meu undefined é: ', typeof meuUndefined)
